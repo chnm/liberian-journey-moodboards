92994e660a4a3517a9c272f23c250ebf8067235c
kalbers <ken.albers@gmail.com>
2014-09-04 14:40:19 -0400

    Updated moodboards.

--- index.php
+++ index.php
@@ -1,151 +1,67 @@
 <?php include('header.php'); ?>
-    
+
     <div class="row">
       <div class="large-12 columns">
-        <h1>Liberian Journey Moodboards</h1>
+        <h1>Liberian Journey Moodboard</h1>
       </div>
     </div>
 
-    <div class="row">
-    <div class="row">
-      <div class="large-12 columns">
-          <p>Select a tab to change the moodboard:</p>   
-          <ul class="tabs" data-tab data-options="deep_linking:true; scroll_to_content: false">
-            <li class="tab-title active"><a href="#historical">Historical</a></li>
-            <li class="tab-title"><a href="#dark">Dark</a></li>
-            <li class="tab-title"><a href="#open">Open</a></li>
-          </ul>
-       </div>
-       </div>
-    </div>
-
-
-    <div class="tabs-content">
-
-    <div class="content active" id="historical">
-
-      <div class="row">
+          <div id="open" class="row content">
 
         <div class="large-12 columns">
-          <h2>Historical</h2>
-          <p>Muted colors. Emphasizing the narrative of the past.</p>
+          <h2>Open</h2>
+          <p>Inviting look, emphasis on sharing content.</p>
         </div>
 
         <div class="large-12 columns section first">
           <h3>Inspiration</h3>
         </div>
 
-            <div class="large-6 left">
-              <div class="large-6 columns">
-                <img src="img/stamp.jpg" alt="">
-              </div>
-
-              <div class="large-6 columns">
-                <img src="img/firestoneAd.jpg" alt="">
-              </div>
-            </div>
-
-          
-
-          <div class="large-6 right columns"><img src="img/robe.jpg" alt=""></div>
-
-          <div class="large-3 columns top">
-            <img src="img/doll.jpg" alt="">
-          </div>
-
-          <div class="large-3 columns top">
-            <img src="img/house.jpg" alt="">
-          </div>
-
-          <div class="large-3 columns small-top">
-            <img src="img/group.jpg" alt="">
-          </div>
-
-        <div class="large-12 columns section">
-          <h3>Colors</h3>
-        </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/82A480&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/C77829&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/89A4BB&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/715C4D&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/DBC59B&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/858585&text=+" alt="">
-          </div>
-
-        
-
-        <div class="large-12 columns section">
-          <h3>Typography</h3>
-          <div id="historical-typography">
-            <h1>Liberian Journeys</h1>
-            <h2>Subtitle</h2>
-            <h3>Subtitle</h3>
-            <h4>Subtitle</h4>
-            <h5>Subtitle</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis voluptatibus, perspiciatis quos consectetur, assumenda tenetur labore a! Odit iusto nihil itaque, perferendis nobis, labore error exercitationem delectus deleniti, suscipit nemo.</p>
-          </div>
-        </div>
-
-      </div>
-      
-    </div>
-
-
-
-      <div id="dark" class="row content">
-
-        <div class="large-12 columns no-bottom">
-          <h2>Dark</h2>
-          <p>A gritter feel. Not indicative of a particlar timeframe.</p>
-        </div>
-
-        <div>
-          
-        </div>
-        <div class="large-12 columns section first">
-          <h3>Inspiration</h3>
-        
-
-          <div class="large-3 columns" id="trees">
-            <img src="img/rubber_tree.jpg" alt="">
-            <img src="img/grove.jpg" alt="">
-            <img src="img/rubberharvest.jpg" alt="">
-          </div>
-
-          <div class="large-3 columns">
-            <img src="img/hat.jpg">
-            <img src="img/waistbeads.jpg" alt="">
-          </div>
-
-          <div class="large-6 right columns">
-            <img src="img/hall.jpg" alt="">
-          </div>
-
-
-          <div class="large-6 right water columns">
+          <div class="large-4 columns street">
+            <img src="img/street.jpg" alt="">
+            <img src="img/dance.jpg" alt="">
             <img src="img/water.jpg" alt="">
           </div>
-        </div>
+
+          <div class="large-8 columns water">
+            
+            <img src="img/grove.jpg" alt="">
+            
+          </div>
+
+          <div class="large-2 columns seal">
+            <!-- <img src="img/seal.png" alt=""> -->
+            <img src="img/childdance.jpg" alt="">
+<!--             <img src="img/web.jpg" alt="">
+            <img src="img/mesurado.gif" alt=""> -->
+          </div>
+
+          <div class="large-2 columns seal">
+
+            <img src="img/web.jpg" alt="">
+
+          </div>
+
+          <div class="large-2 columns seal">
+
+            <img src="img/mesurado.gif" alt="">
+          </div>
+
+          <div class="large-2 columns seal">
+            <img src="img/seal.png" alt="">
+
+          </div>
+
+
 
         <div class="large-12 columns section">
           <h3>Colors</h3>
+        </div>
+
+
+          <div class="large-2 columns">
+            <img class="color" src="http://placehold.it/200x200/FCC32B&text=+" alt="">
+          </div>
         
 
           <div class="large-2 columns">
@@ -168,11 +84,10 @@
             <img class="color" src="http://placehold.it/200x200/212121&text=+" alt="">
           </div>
 
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/CCCCCC&text=+" alt="">
-          </div>
 
-      </div>
+      
+
+        
 
         <div class="large-12 columns section">
           <h3>Typography</h3>
@@ -184,83 +99,7 @@
             <h5>Subtitle</h5>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis voluptatibus, perspiciatis quos consectetur, assumenda tenetur labore a! Odit iusto nihil itaque, perferendis nobis, labore error exercitationem delectus deleniti, suscipit nemo.</p>
           </div>
-        </div>
-
-      </div>
-
-      <div id="open" class="row content">
-
-        <div class="large-12 columns">
-          <h2>Open</h2>
-          <p>Inviting look, emphasis on sharing content.</p>
-        </div>
-
-        <div class="large-12 columns section first">
-          <h3>Inspiration</h3>
-        </div>
-
-          <div class="large-4 columns street">
-            <img src="img/street.jpg" alt="">
-            <img src="img/dance.jpg" alt="">
-          </div>
-
-          <div class="large-6 columns">
-            <img src="img/mesurado.gif" alt="">
-          </div>
-
-          <div class="large-2 columns seal">
-            <img src="img/seal.png" alt="">
-            <img src="img/childdance.jpg" alt="">
-            <img src="img/web.jpg" alt="">
-          </div>
-
-
-        <div class="large-12 columns section">
-          <h3>Colors</h3>
-        </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/FCDF16&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/F9B222&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/3BFA9F&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/4A6A91&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/7B0FAC&text=+" alt="">
-          </div>
-
-          <div class="large-2 columns">
-            <img class="color" src="http://placehold.it/200x200/E5415C&text=+" alt="">
-          </div>
-
-        
-
-        <div class="large-12 columns section">
-          <h3>Typography</h3>
-          <div id="open-typography">
-            <h1>Liberian Journeys</h1>
-            <h2>Subtitle</h2>
-            <h3>Subtitle</h3>
-            <h4>Subtitle</h4>
-            <h5>Subtitle</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis voluptatibus, perspiciatis quos consectetur, assumenda tenetur labore a! Odit iusto nihil itaque, perferendis nobis, labore error exercitationem delectus deleniti, suscipit nemo.</p>
-          </div>
 
         </div>
 
       </div>
-      
-
-
-    </div>
-<?php include('footer.php'); ?>
